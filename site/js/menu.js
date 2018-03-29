ce8e8feb3cf44688a5dc1ff16d95b70c307fc884
itggot-eric-jigrup <eric.jigrup@itggot.se>
2018-03-29 09:40:48 +0200

    tagit bort css och lagt till photoshopada bilder

---
+++ site/js/menu.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    var element = document.querySelector(".menu");
+    element.classList.toggle("show");
+
+    element = document.querySelector("main");
+    element.classList.toggle("fade")
+
+    
+}
